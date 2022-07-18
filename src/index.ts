71506e97771d64ea36ded0c6704218333e052daf
AAGaming <aa@mail.catvibers.me>
2022-07-17 23:43:09 -0400

    updates for rollup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { Plugin } from "ittai/entities";
 import * as React from "react";
 import ExampleSettingsPage from "./components/Settings";
-import "./bruh.css";
 export default class ExamplePlugin extends Plugin {
     start() {
         this.setSettingsPanel(() => React.createElement(ExampleSettingsPage));
-        console.log("hello (after a hot reload)!");
+        console.log("hello from plugin!");
         console.log(this);
     }
 
     stop() {
-        console.log("bye");
+        console.log("goodbye from plugin!");
     }
 }
